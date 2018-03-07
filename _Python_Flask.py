ce7d21f4d77270e06612a2940a16692b24c36ec0
bogucek4 <bogucek4@gazeta.pl>
2018-03-07 23:13:31 +0100

    FourthCommit

--- _Python_Flask.py
+++ _Python_Flask.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! #MySecondComment '
+    return 'Hello World! #MySecondComment #MyFirstComment#MyFirstComment '
 
-#MyFirstComment
-
-#MySecondComment
+#MyFirstComment#MyFirstComment#MyFirstComment#MyFirstComment#MyFirstComment
+#MyFirstComment#MyFirstComment#MyFirstComment#MyFirstComment#MyFirstComment
+#MySecondComment#MyFirstComment#MyFirstComment#MyFirstComment#MyFirstComment
 
 if __name__ == '__main__':
     app.run()
